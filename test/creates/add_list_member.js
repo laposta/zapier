0710b7eec9167141a08884bfc1d0397b968f6497
Jan den Besten <info@jandenbesten.net>
2021-01-05 15:07:57 +0100

    Test add_list_member werkend (met random data)

--- test/creates/add_list_member.js
+++ test/creates/add_list_member.js
@@ -5,17 +5,28 @@ const zapier = require('zapier-platform-core');
 const App = require('../../index');
 const appTester = zapier.createAppTester(App);
 
+const Str = require('@supercharge/strings');
+const randomData = {
+  email : Str.random(10) + '@laposta.tester.nl',
+  firstname : Str.random(1).toUpperCase() + Str.random(10),
+  lastname : 'Laposta Tester',
+};
+
 describe('Create - add_list_member', () => {
   zapier.tools.env.inject();
 
   it('should create an object', async () => {
     const bundle = {
       authData: {
-        username: process.env.USERNAME,
-        password: process.env.PASSWORD,
+        username: process.env.API_KEY,
       },
-
-      inputData: {},
+      inputData: {
+        list_id: process.env.LIST_ID,
+        ip: process.env.IP,
+        email: randomData.email,
+        voornaam: randomData.firstname,
+        achternaam: randomData.lastname,
+      },
     };
 
     const result = await appTester(
