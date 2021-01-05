de31ef43338745141e3a0da1e422cd4a2c6fb32f
Jan den Besten <info@jandenbesten.net>
2021-01-05 13:47:38 +0100

    rewrite App

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const authentication = require('./authentication');
 const addListMemberCreate = require('./creates/add_list_member.js');
 
-module.exports = {
+const recipeResource = require("./resources/recipe");
+
+const App = {
   version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
   authentication: authentication,
+
   creates: {
     [addListMemberCreate.key]: addListMemberCreate
   },
+
+  // beforeRequest:[],
+  // afterResponse:[],
   triggers: {
 
   },
-  // beforeRequest:[],
-  // afterResponse:[],
+
+  resources: {
+    [recipeResource.key]: recipeResource
+  }
 };
+
+
+module.exports = App;
