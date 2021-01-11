4bdb41ee8648bb44bdd0cf2bae76b4cec343c7b7
Jan den Besten <info@jandenbesten.net>
2021-01-11 15:03:02 +0100

    bezig met webhook

--- test/authentication.js
+++ test/authentication.js
@@ -1,26 +1,26 @@
-require('should');
+// require('should');
 
-const zapier = require('zapier-platform-core');
+// const zapier = require('zapier-platform-core');
 
-const App = require('../index');
-const appTester = zapier.createAppTester(App);
+// const App = require('../index');
+// const appTester = zapier.createAppTester(App);
 
-describe('Test Authentication', () => {
-  zapier.tools.env.inject();
+// describe('Test Authentication', () => {
+//   zapier.tools.env.inject();
 
-  it('should return no 401', async () => {
-    const bundle = {
-      authData: {
-        // username: process.env.API_KEY,
-        api_key: process.env.API_KEY,
-      },
-      inputData: {
-      },
-    };
-    const result = await appTester(
-      App.authentication.test,
-      bundle
-    );
-    result.should.not.be.an.Array();
-  });
-});
+//   it('should return no 401', async () => {
+//     const bundle = {
+//       authData: {
+//         // username: process.env.API_KEY,
+//         api_key: process.env.API_KEY,
+//       },
+//       inputData: {
+//       },
+//     };
+//     const result = await appTester(
+//       App.authentication.test,
+//       bundle
+//     );
+//     result.should.not.be.an.Array();
+//   });
+// });
--- triggers/added_list_member.js
+++ triggers/added_list_member.js
@@ -1,20 +1,18 @@
-const subscribeHook = (z, bundle) => {
-  // bundle.targetUrl has the Hook URL this app should call when a recipe is created.
-  const data = {
-    url: bundle.targetUrl,
-    style: bundle.inputData.style,
-  };
+// const subscribeHook = (z, bundle) => {
+//   // bundle.targetUrl has the Hook URL this app should call when a recipe is created.
+//   const hookUrl = bundle.targetUrl;
+//   const data = bundle.inputData;
 
-  // You can build requests and our client will helpfully inject all the variables
-  // you need to complete. You can also register middleware to control this.
-  const options = {
-    url: 'https://57b20fb546b57d1100a3c405.mockapi.io/api/hooks',
-    method: 'POST',
-    body: data,
-  };
-  // You may return a promise or a normal data structure from any perform method.
-  return z.request(options).then((response) => response.data);
-};
+//   // You can build requests and our client will helpfully inject all the variables
+//   // you need to complete. You can also register middleware to control this.
+//   const options = {
+//     url: hookUrl,
+//     method: 'POST',
+//     body: data,
+//   };
+//   // You may return a promise or a normal data structure from any perform method.
+//   return z.request(options).then((response) => response.data);
+// };
 
 // const unsubscribeHook = (z, bundle) => {
 //   // bundle.subscribeData contains the parsed response JSON from the subscribe
@@ -32,20 +30,12 @@ const subscribeHook = (z, bundle) => {
 //   return z.request(options).then((response) => response.data);
 // };
 
-// const getRecipe = (z, bundle) => {
-//   // bundle.cleanedRequest will include the parsed JSON object (if it's not a
-//   // test poll) and also a .querystring property with the URL's query string.
-//   const recipe = {
-//     id: bundle.cleanedRequest.id,
-//     name: bundle.cleanedRequest.name,
-//     directions: bundle.cleanedRequest.directions,
-//     style: bundle.cleanedRequest.style,
-//     authorId: bundle.cleanedRequest.authorId,
-//     createdAt: bundle.cleanedRequest.createdAt,
-//   };
-
-//   return [recipe];
-// };
+const getRecipe = (z, bundle) => {
+  // bundle.cleanedRequest will include the parsed JSON object (if it's not a
+  // test poll) and also a .querystring property with the URL's query string.
+  const recipe = bundle.cleanedRequest;
+  return [recipe];
+};
 
 // const getFallbackRealRecipe = (z, bundle) => {
 //   // For the test poll, you should get some real data, to aid the setup process.
@@ -62,7 +52,7 @@ const subscribeHook = (z, bundle) => {
 
 // Main
 const AdddedListMember = {
-  key: 'addded_list_member',
+  key: 'added_list_member',
   noun: 'Relatie',
   display: {
     label: 'Een Nieuwe Relatie is Toegevoegd',
@@ -72,9 +62,9 @@ const AdddedListMember = {
   },
   operation: {
     type: 'hook',
-    performSubscribe: subscribeHook,
+    perform: getRecipe,
+    // performSubscribe: subscribeHook,
     // performUnsubscribe: unsubscribeHook,
-    // perform: getRecipe,
     // performList: getFallbackRealRecipe,
     sample: {
       sample: {
@@ -85,30 +75,9 @@ const AdddedListMember = {
 
   // `inputFields` can define the fields a user could provide,
   // we'll pass them in as `bundle.inputData` later.
-  inputFields: [
-  ],
-  outputFields: [
-  ],
+  // inputFields: [],
+  // outputFields: [],
 
-
-
-  //   perform: (z, bundle) => {
-  //     // let body     = bundle.inputData;
-  //     // body.list_id = bundle.inputData.list_id;
-  //     // body.ip      = '0.0.0.0';
-  //     const promise = z.request({
-  //       url: 'https://api.laposta.nl/v2/member',
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/x-www-form-urlencoded',
-  //         Accept: 'application/json',
-  //       },
-  //       body: body,
-  //       removeMissingValuesFrom: {},
-  //     });
-  //     return promise.then((response) => response.data);
-  //   },
-  // },
 };
 
 module.exports = AdddedListMember;
